a5582a05f7c1d48a4773853b54c890e120a9234c
Eugen Dueck <eugen@dueck.org>
2024-05-20 04:35:42 +0900

    copy csv and download svg buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import sampleDiagramSource from "./sample-diagram-source.ts";
 import githubMark from './assets/github-mark.svg'
 
 function App() {
+    const copyBtnLabel = 'Copy SVG';
     const textPlaceholder = `Enter a bytefield-svg diagram here`
     const [text, setText] = createSignal(sampleDiagramSource);
     const [svg, setSvg] = createSignal(generateDiagram(text()));
+    const [copyButtonLabel, setCopyButtonLabel] = createSignal(copyBtnLabel);
 
     function generateDiagram(diagramSource: string): string {
         return generate(diagramSource, {"embedded": true});
@@ -20,6 +22,32 @@ function App() {
         setSvg(generateDiagram(newDiagramSource))
     }
 
+    const downloadSvg = () => {
+        const svgBlob = new Blob([svg()], { type: 'image/svg+xml;charset=utf-8' });
+        const url = URL.createObjectURL(svgBlob);
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'downloaded.svg';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+        // Cleanup URL object
+        URL.revokeObjectURL(url);
+    };
+
+    const copySvg = async () => {
+        try {
+            await navigator.clipboard.writeText(svg());
+            setCopyButtonLabel('Copied!');
+        } catch (err) {
+            console.error('Failed to copy: ', err);
+            setCopyButtonLabel('Error!');
+        }
+        setTimeout(() => setCopyButtonLabel(copyBtnLabel), 1000);
+    };
+
     return (
         <div class="app-container">
             <h4 class="title">
@@ -31,6 +59,8 @@ function App() {
                 <span>ByteField-SVG Studio </span>
                 <a target="_blank" href="https://bytefield-svg.deepsymmetry.org/bytefield-svg/intro.html">(ByteField
                     Format Docs)</a>
+                <button onClick={copySvg}>{copyButtonLabel()}</button>
+                <button onClick={downloadSvg}>Download SVG</button>
             </h4>
             <textarea
                 class="source"
